697811795f82f1b398df58313aa43534459a95df
Matt Casper <matthewvcasper@gmail.com>
2016-11-24 10:28:28 -0800

    Finish up git-clean recipe

--- Formula/git-clean.rb
+++ Formula/git-clean.rb
@@ -1,14 +1,17 @@
 class GitClean < Formula
-  desc "A command line tool for cleaning up local git branches."
+  desc "Command-line tool for cleaning up local git branches."
   homepage "https://github.com/mcasper/git-clean"
   url "https://github.com/mcasper/git-clean/archive/0.4.4.tar.gz"
-  version "0.4.4"
-  sha256 ""
+  sha256 "39404bbc325c59ff1d7265e91cec7c750fec60df7f8c2c410709f312ff564176"
   head "https://github.com/mcasper/git-clean.git"
 
   depends_on "rust" => :build
 
   def install
     system("cargo", "install", "git-clean", "--force")
+  end
+
+  test do
+    system "#{bin}/git-clean", "-h"
   end
 end
