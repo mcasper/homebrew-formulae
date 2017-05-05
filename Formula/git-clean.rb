1ac2c6fee40235bcb7b9881fec51e784dc81ed98
Matt Casper <matthewvcasper@gmail.com>
2017-05-05 12:28:31 -0700

    Fix the 0.5.1 sha

--- Formula/git-clean.rb
+++ Formula/git-clean.rb
@@ -2,7 +2,7 @@ class GitClean < Formula
   desc "Command-line tool for cleaning up local git branches."
   homepage "https://github.com/mcasper/git-clean"
   url "https://github.com/mcasper/git-clean/archive/0.5.1.tar.gz"
-  sha256 "001c582cec41119e5bfcfc860fa163ea444ee4bafc9cbbbb9ca68cc414908f58"
+  sha256 "d68ca8c3b71ad78556b7b43e1cf8ea50a2df376f7e7e5780388c6de5ee70ee59"
   head "https://github.com/mcasper/git-clean.git"
 
   depends_on "rust" => :build
