a1074b79b08152e1c84220c30405ed12dab14038
Matt Casper <matthewvcasper@gmail.com>
2019-06-11 11:04:23 +0200

    Update formula to latest version

--- Formula/git-clean.rb
+++ Formula/git-clean.rb
@@ -1,8 +1,8 @@
 class GitClean < Formula
   desc "Command-line tool for cleaning up local git branches."
   homepage "https://github.com/mcasper/git-clean"
-  url "https://github.com/mcasper/git-clean/archive/0.5.1.tar.gz"
-  sha256 "d68ca8c3b71ad78556b7b43e1cf8ea50a2df376f7e7e5780388c6de5ee70ee59"
+  url "https://github.com/mcasper/git-clean/archive/0.5.2.tar.gz"
+  sha256 "c1d255dff44532b1d6f0388dd34dd76acc77f043417aa6bc83e02ecfcbf672c3"
   head "https://github.com/mcasper/git-clean.git"
 
   depends_on "rust" => :build
