b073fd71f745e29b50f36daea126c8afa1e54bc3
Matt Casper <mattcasper@MacBook-Pro.local>
2020-06-26 11:37:24 -0700

    Update git-clean to latest patch

--- Formula/git-clean.rb
+++ Formula/git-clean.rb
@@ -1,8 +1,8 @@
 class GitClean < Formula
   desc "Command-line tool for cleaning up local git branches."
   homepage "https://github.com/mcasper/git-clean"
-  url "https://github.com/mcasper/git-clean/archive/0.5.2.tar.gz"
-  sha256 "c1d255dff44532b1d6f0388dd34dd76acc77f043417aa6bc83e02ecfcbf672c3"
+  url "https://github.com/mcasper/git-clean/archive/0.5.3.tar.gz"
+  sha256 "7323f837a0884053f4f885dfd24d0172aa3a105a6a1e610d920b880ecc6da7b1"
   head "https://github.com/mcasper/git-clean.git"
 
   depends_on "rust" => :build
